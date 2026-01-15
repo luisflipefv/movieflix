7766fb3423292eb631b89a281186590cd985c074
luisf <luisfelipefv8@hotmail.com>
2026-01-15 18:13:43 -0300

    adiciona anotações Swagger para documentação do UserResponse

--- src/main/java/dev/luisf/movieflix/Controller/response/UserResponse.java
+++ src/main/java/dev/luisf/movieflix/Controller/response/UserResponse.java
@@ -1,7 +1,15 @@
 package dev.luisf.movieflix.Controller.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record UserResponse (Long id, String name, String email) {
+public record UserResponse (@Schema(type = "integer", description = "Id do usuario")
+                            Long id,
+
+                            @Schema(type = "string", description = "Nome do usuario")
+                            String name,
+
+                            @Schema(type = "string", description = "Email do usuario")
+                            String email) {
 }
