290b98dfe140fd431ed1f8298e766e2230701573
luisf <luisfelipefv8@hotmail.com>
2026-01-15 18:14:26 -0300

    adiciona anotações Swagger para documentação do CategoryController

--- src/main/java/dev/luisf/movieflix/Controller/CategoryController.java
+++ src/main/java/dev/luisf/movieflix/Controller/CategoryController.java
@@ -2,9 +2,17 @@ package dev.luisf.movieflix.Controller;
 
 import dev.luisf.movieflix.Controller.request.CategoryRequest;
 import dev.luisf.movieflix.Controller.response.CategoryResponse;
+import dev.luisf.movieflix.Controller.response.MovieResponse;
 import dev.luisf.movieflix.entity.Category;
 import dev.luisf.movieflix.mapper.CategoryMapper;
 import dev.luisf.movieflix.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,10 +24,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/movieflix/category")
 @RequiredArgsConstructor
+@Tag(name = "Category", description = "Recurso responsavel pelo gerenciamento das categorias.")
 public class CategoryController {
 
     private final CategoryService categoryService;
 
+    @Operation(summary = "Buscar todas as categorias", description = "Metodo responsavel por retornar a lista de todas as categorias cadastradas", security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Retorna todas as categorias cadastradas", content = @Content(array = @ArraySchema(schema = @Schema(implementation = CategoryResponse.class))))
     @GetMapping
     public ResponseEntity<List<CategoryResponse>> getAllCategories(){
         return ResponseEntity.ok(categoryService.findAll()
@@ -28,6 +39,8 @@ public class CategoryController {
             .toList());
     }
 
+    @Operation(summary = "Salvar categoria", description = "metodo responsavel pelo salvamento de uma nova categoria", security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "201", description = "Categoria criada com sucesso", content = @Content(schema = @Schema(implementation = CategoryResponse.class)))
     @PostMapping
     public ResponseEntity<CategoryResponse> saveCategory(@Valid @RequestBody CategoryRequest request){
         Category newCategory = CategoryMapper.toCategory(request);
@@ -36,6 +49,9 @@ public class CategoryController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(summary = "Buscar categoria por id", description = "metodo responsavel por retornar uma categoria baseada em no id", security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Categoria encontrado com sucesso", content = @Content(schema = @Schema(implementation = CategoryResponse.class)))
+    @ApiResponse(responseCode = "404", description = "Categoria nao encontrado")
     @GetMapping("/{id}")
     public ResponseEntity<CategoryResponse> getByCategoryId(@PathVariable Long id){
         return categoryService.getCategoryById(id)
@@ -43,6 +59,9 @@ public class CategoryController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Deletar categoria por id", description = "metodo responsavel por deletar dados de uma categoria por id", security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "204", description = "Categoria deletada com sucesso")
+    @ApiResponse(responseCode = "404", description = "Categoria nao encontrado")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteByCategoryId(@PathVariable Long id){
         categoryService.deleteCategory(id);
