755950c172a366fb6d13592fdad72ca0ef6619a3
luisf <luisfelipefv8@hotmail.com>
2026-01-15 18:13:47 -0300

    adiciona anotações Swagger para documentação do UserRequest

--- src/main/java/dev/luisf/movieflix/Controller/request/UserRequest.java
+++ src/main/java/dev/luisf/movieflix/Controller/request/UserRequest.java
@@ -1,7 +1,15 @@
 package dev.luisf.movieflix.Controller.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record UserRequest(String name, String email, String password) {
+public record UserRequest(@Schema(type = "string", description = "Nome do usuario")
+                          String name,
+
+                          @Schema(type = "string", description = "Email do usuario")
+                          String email,
+
+                          @Schema(type = "string", description = "Senha do usuario")
+                          String password) {
 }
