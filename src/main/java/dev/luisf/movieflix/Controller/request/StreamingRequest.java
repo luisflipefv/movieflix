345d226badc3891c6b79d86270174c6661611366
luisf <luisfelipefv8@hotmail.com>
2026-01-15 18:13:56 -0300

    adiciona anotações Swagger para documentação do StreamingRequest

--- src/main/java/dev/luisf/movieflix/Controller/request/StreamingRequest.java
+++ src/main/java/dev/luisf/movieflix/Controller/request/StreamingRequest.java
@@ -1,8 +1,11 @@
 package dev.luisf.movieflix.Controller.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
 @Builder
-public record StreamingRequest(@NotEmpty(message = "Nome do streaming obrigatorio") String name) {
+public record StreamingRequest(@Schema(type = "string", description = "Nome do filme")
+                               @NotEmpty(message = "Nome do streaming obrigatorio")
+                               String name) {
 }
