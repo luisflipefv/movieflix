23dff47e54e80543321002a85fc450f317c96cc2
luisf <luisfelipefv8@hotmail.com>
2026-01-15 18:13:51 -0300

    adiciona anotações Swagger para documentação do StreamingResponse

--- src/main/java/dev/luisf/movieflix/Controller/response/StreamingResponse.java
+++ src/main/java/dev/luisf/movieflix/Controller/response/StreamingResponse.java
@@ -1,7 +1,12 @@
 package dev.luisf.movieflix.Controller.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record StreamingResponse(Long id, String name) {
+public record StreamingResponse(@Schema(type = "integer", description = "Id do streaming")
+                                Long id,
+
+                                @Schema(type = "string", description = "Nome do streaming")
+                                String name) {
 }
