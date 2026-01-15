f956210dcef33ea9b9a06c1121981bdb3974bda9
luisf <luisfelipefv8@hotmail.com>
2026-01-15 18:14:19 -0300

    adiciona anotações Swagger para documentação do CategoryResponse

--- src/main/java/dev/luisf/movieflix/Controller/response/CategoryResponse.java
+++ src/main/java/dev/luisf/movieflix/Controller/response/CategoryResponse.java
@@ -1,7 +1,12 @@
 package dev.luisf.movieflix.Controller.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record CategoryResponse(Long id, String name) {
+public record CategoryResponse(@Schema(type = "integer", description = "Id da categoria")
+                               Long id,
+
+                               @Schema(type = "string", description = "Nome da categoria")
+                               String name) {
 }
