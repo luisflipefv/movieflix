a7e9fb4cbf07dc67343ae073c5a930924e879cb2
luisf <luisfelipefv8@hotmail.com>
2026-01-15 18:14:07 -0300

    adiciona anotações Swagger para documentação do MovieRequest

--- src/main/java/dev/luisf/movieflix/Controller/request/MovieRequest.java
+++ src/main/java/dev/luisf/movieflix/Controller/request/MovieRequest.java
@@ -1,6 +1,7 @@
 package dev.luisf.movieflix.Controller.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
@@ -8,13 +9,23 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Builder
-public record MovieRequest(@NotEmpty(message = "O titulo do filme e obrigatorio") String title,
+public record MovieRequest(@Schema(type = "string", description = "Nome do filme")
+                           @NotEmpty(message = "O titulo do filme e obrigatorio")
+                           String title,
+
+                           @Schema(type = "string", description = "Sinopse do filme")
                            String description,
 
+                           @Schema(type = "date", description = "Data de lançamento do filme")
                            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
                            LocalDate release,
 
+                           @Schema(type = "double", description = "Score do filme")
                            double rating,
+
+                           @Schema(type = "array", description = "Lista de codigos de categorias")
                            List<Long> categories,
+
+                           @Schema(type = "array", description = "Lista de condigos de streamings")
                            List<Long> streamings) {
 }
