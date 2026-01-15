699e33d91e8536c99c3393aeea59ba72c80df40f
luisf <luisfelipefv8@hotmail.com>
2026-01-15 18:14:23 -0300

    adiciona anotações Swagger para documentação do CategoryRequest

--- src/main/java/dev/luisf/movieflix/Controller/request/CategoryRequest.java
+++ src/main/java/dev/luisf/movieflix/Controller/request/CategoryRequest.java
@@ -1,8 +1,11 @@
 package dev.luisf.movieflix.Controller.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
 @Builder
-public record CategoryRequest(@NotEmpty(message = "Nome da categoria obrigatorio") String name) {
+public record CategoryRequest(@Schema(type = "string", description = "Nome da categoria")
+                              @NotEmpty(message = "Nome da categoria obrigatorio")
+                              String name) {
 }
