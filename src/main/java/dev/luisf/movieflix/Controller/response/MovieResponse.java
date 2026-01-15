b90a6888f618f7fb2c01d065aaf6fe162fb90e31
luisf <luisfelipefv8@hotmail.com>
2026-01-15 18:14:03 -0300

    adiciona anotações Swagger para documentação do MovieResponse

--- src/main/java/dev/luisf/movieflix/Controller/response/MovieResponse.java
+++ src/main/java/dev/luisf/movieflix/Controller/response/MovieResponse.java
@@ -1,20 +1,32 @@
 package dev.luisf.movieflix.Controller.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Builder
-public record MovieResponse(Long id,
-                            String tile,
+public record MovieResponse(@Schema(type = "integer", description = "Id do filme")
+                            Long id,
+
+                            @Schema(type = "string", description = "Nome do filme")
+                            String title,
+
+                            @Schema(type = "string", description = "Sinopse do filme")
                             String description,
 
+                            @Schema(type = "date", description = "Data de lançamento do filme")
                             @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
                             LocalDate releaseDate,
 
+                            @Schema(type = "double", description = "Score do filme")
                             double rating,
+
+                            @Schema(type = "array", description = "Lista de codigos de categorias")
                             List<CategoryResponse> categories,
+
+                            @Schema(type = "array", description = "Lista de condigos de streamings")
                             List<StreamingResponse> streamings) {
 }
